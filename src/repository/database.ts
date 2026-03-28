6c95974716b3ca6e0e107c242bf39818c824cb35
micjaelajanceova <micjan01@easv365.dk>
2026-03-28 14:59:34 +0100

    test

--- src/repository/database.ts
+++ src/repository/database.ts
@@ -1,49 +1,32 @@
 import mongoose from "mongoose";
 
-
-export  async function  testConnection() {
+export async function testConnection() {
     try {
         await connect();
         await disconnect();
         console.log("Database connection test successful");
-    }
-    catch (error) {
+    } catch (error) {
         console.log("Database connection test failed. Error: " + error);
+        throw error;
     }
 }
 
-
-export async function connect () {
-    try 
-    {
-        if (!process.env.DBHOST) {
-            throw new Error("DBHOST is not defined in environment variables");
-        }
-        await mongoose.connect (process.env.DBHOST);
-
-        if (mongoose.connection.db) {
-            await mongoose.connection.db.admin().command({ping: 1});
-            console.log("Connected to the database successfully");
-        }
-        else {
-            throw new Error("Failed to connect to the database");
-    
-        }
+export async function connect() {
+    if (!process.env.DBHOST) {
+        throw new Error("DBHOST is not defined in environment variables");
     }
-    catch (error)
-    {
-        console.log("Error connecting to the database. Error:" + error);
+
+    await mongoose.connect(process.env.DBHOST);
+
+    if (!mongoose.connection.db) {
+        throw new Error("Failed to connect to the database");
     }
+
+    await mongoose.connection.db.admin().command({ ping: 1 });
+    console.log("Connected to the database successfully");
 }
 
-export async function disconnect () {
-    try 
-    {
-        await mongoose.disconnect();
-        console.log("Disconnected from the database successfully");
-    }
-    catch (error)
-    {
-        console.log("Error disconnecting from the database. Error:" + error);
-    }
+export async function disconnect() {
+    await mongoose.disconnect();
+    console.log("Disconnected from the database successfully");
 }
